f2c0026fc62be5674d7fb0bb2d8b7bc0a2b11348
Jyoti Vishwakarma <vishwakarmajyoti65@gmail.com>
2025-06-23 00:27:39 +0530

    TMS

---
+++ frontend/src/Pages/UserDashboard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
+const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div id="topdash">
+        <h1>
+          Welcome! User <BsFillEmojiSmileFill />
+        </h1>
+      </div>
+
+      <div id="mainPG">
+        <div id="topvar">
+          <h2>Dashboard!</h2>
+          <Button id="btn2" onClick={logout}>
+            Log out!
+          </Button>
+        </div>
+        <div className="sidebar">
+          <section id="dividedash">
+            <div id="sidevar">
+              <ul>
+                <li>Overview</li>
+                <li>
+                  <Link to="usertask" id="link">
+                    My Task
+                  </Link>
+                </li>
+                <li> 
+                  <Link to="changepass" id="link">Change Password</Link></li>
+                <li>Services</li>
+                <li>Logout</li>
+              </ul>
+            </div>
+          </section>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
